d9a5ba80cb7365c162092ef339efabf4924f7942
Jeel <jeel.p@simformsolutions.com>
2023-04-25 18:02:26 +0530

    [feat: add product route]

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import userRouter from './user/index.js'
 import authRoute from "./auth/index.js"
+import productRouter from "./product/index.js"
 
 const route = express.Router()
 
 route.use("/auth", authRoute)
 route.use("/user", userRouter)
+route.use("/product", productRouter)
 
 export default route
